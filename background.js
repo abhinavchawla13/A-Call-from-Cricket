3614bd128853d7a165703b3f8dbe4ba7c71168c5
abhinavchawla13 <abhinavchawla13@yahoo.com>
2015-12-05 17:46:15 -0500

    Minor Changes

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(){
-  var newURL = chrome.extension.getURL("bg.html");
-  chrome.tabs.create({ url: newURL }, function(tab){
-  });
-});
--- js/popup.js
+++
@@ -1,7 +0,0 @@
-// create a list of players in a team
-function teamPlayers(id){
-  windows.alert("hey")
-  var team_players = []
-
-
-}
